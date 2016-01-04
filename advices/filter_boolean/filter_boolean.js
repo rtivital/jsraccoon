3c4b780e78fd96191fdf6e0d303865f7ba9edf02
rtivital <rtivital@gmail.com>
2016-01-04 23:07:58 +0300

    Initial Site commit

--- advices/filter_boolean/filter_boolean.js
+++
@@ -1,7 +0,0 @@
-var arr = ['', 1, 'str 1', 0, false, 'str 2', null];
-arr.filter(Boolean); // [1, 'str1', 'str 2']
-
-// Сокращенный вариант функции 
-arr.filter(function(item) {
-	return !!item;
-});
--- exercises/earthquake/earthquake_3.js
+++
@@ -1,3 +0,0 @@
-function earthquake(quake, year) {
-  return quake.map(wave => wave.reduce((a, b) => a + b)).reduce((a, b) => a * b) - 1000 * Math.pow(0.98, new Date().getFullYear() - year) > 10
-}
