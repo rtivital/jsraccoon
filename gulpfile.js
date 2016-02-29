8330a7394f182bf6224b93f76614556a54b9f049
rtivital <rtivital@gmail.com>
2016-02-29 13:20:07 +0300

    final redesign

--- gulpfile.js
+++ gulpfile.js
@@ -2,47 +2,31 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const minifyCss = require('gulp-minify-css');
 const rename = require('gulp-rename');
 const filter = require('gulp-filter');
 const scsslint = require('gulp-scss-lint');
 
-const paths = {
-	sass: {
-			all: './styles/**/*.scss',
-			lint: '/styles/vendor/**/*.scss',
-			lintConfig: './scsslint.yml'
-		},
-	dist: {
-		css: './css',
-		js: './js'
-	}
-};
-
-// paths.sass.exclude = paths.sass.exclude.map(path => `!${path}`);
+const lintScss = [
+  './styles/site/*.scss',
+  './styles/mixins/*.scss',
+  './styles/typography/*.scss',
+  './styles/variables/*.scss',
+  './styles/main.scss'
+];
 
 gulp.task('lint:scss', () => {
-	// const files = [...paths.sass.files, ...paths.sass.exclude];
-	// console.log(files, paths.sass);
-	const exclude = filter(['*', '!styles/modules'], {restore: true});
-	return gulp.src(['./styles/vendor/**/*.scss'])
-		// .pipe(exclude.restore)
+	return gulp.src(lintScss)
 		.pipe(scsslint({
   		config: './scsslint.yml'
   	}));
-
 });
 
-gulp.task('build:css', () => {
-	// Filter for scss-lint: exclude all third-party libraries
-	const exclude = filter(['styles/**/*.scss', '!modules/**/*.scss'], { restore: true });
+gulp.task('build:css', ['lint:scss'], () => {
   return gulp.src('./styles/**/*.scss')
-  	.pipe(exclude)
-  	.pipe(scsslint({
-  		config: './scsslint.yml'
-  	}))
-  	.pipe(exclude.restore)
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
 			browsers: ['last 4 versions'],
@@ -50,6 +34,7 @@ gulp.task('build:css', () => {
 		}))
 		.pipe(gulp.dest('./css'))
 		.pipe(minifyCss())
+    .pipe(sourcemaps.write())
 		.pipe(rename({ suffix: ".min" }))
     .pipe(gulp.dest('./css'));
 });
